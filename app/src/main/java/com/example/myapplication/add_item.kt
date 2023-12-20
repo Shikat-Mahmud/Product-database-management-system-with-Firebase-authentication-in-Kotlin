24dc55b7e6a3cd57ac4528a7cffbfe85740983b7
Shikat Mahmud <94640470+Shikat-Mahmud@users.noreply.github.com>
2023-12-20 23:29:02 +0600

    add_item activity xml completed...

--- app/src/main/java/com/example/myapplication/add_item.kt
+++ app/src/main/java/com/example/myapplication/add_item.kt
@@ -2,10 +2,20 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
 
 class add_item : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_item)
+
+        val rootRef = FirebaseDatabase.getInstance().reference
+
+//        val usersRef = rootRef.child("users")
+//        val specificUserRef = usersRef.child("user1234")
+
+
     }
 }
