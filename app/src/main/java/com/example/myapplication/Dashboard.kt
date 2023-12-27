7f76ec4d1b4321459d29ccbec2250befa4211988
Shikat Mahmud <94640470+Shikat-Mahmud@users.noreply.github.com>
2023-12-27 21:48:32 +0600

    Progressbar added to the Dashboard fragment.

--- app/src/main/java/com/example/myapplication/Dashboard.kt
+++ app/src/main/java/com/example/myapplication/Dashboard.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -13,25 +14,31 @@ class Dashboard : Fragment() {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: ItemModelAdapter
+    private lateinit var loader: ProgressBar // Add ProgressBar reference
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_dashboard, container, false)
 
         recyclerView = view.findViewById(R.id.itemRecyclerView)
         adapter = ItemModelAdapter(emptyList())
+        loader = view.findViewById(R.id.loader) // Initialize ProgressBar
 
-        recyclerView.layoutManager = LinearLayoutManager(requireContext()) // Use requireContext() instead of this
-
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
         recyclerView.adapter = adapter
 
         val firebaseHelper = FirebaseHelper()
 
+        // Show loader before starting data fetch
+        loader.visibility = View.VISIBLE
+
         firebaseHelper.fetchItemData { itemList ->
             adapter.updateData(itemList)
+
+            // Hide loader after data is loaded
+            loader.visibility = View.GONE
         }
 
         return view
