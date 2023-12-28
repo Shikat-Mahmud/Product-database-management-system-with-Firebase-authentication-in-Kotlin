d9e3c9066392adf3d0722186a205ed1746f3720e
Shikat Mahmud <94640470+Shikat-Mahmud@users.noreply.github.com>
2023-12-29 00:52:33 +0600

    app info page updated.

--- app/src/main/java/com/example/myapplication/info.kt
+++ app/src/main/java/com/example/myapplication/info.kt
@@ -1,11 +1,24 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.MaterialToolbar
 
 class info : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
+
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
