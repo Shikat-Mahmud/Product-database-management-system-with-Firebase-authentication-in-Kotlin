0f9bdc2934d65e5a5294f452428f64eeae7f8f04
Shikat Mahmud <94640470+Shikat-Mahmud@users.noreply.github.com>
2023-12-28 21:47:30 +0600

    Drawer navigation added.

--- app/src/main/java/com/example/myapplication/home.kt
+++ app/src/main/java/com/example/myapplication/home.kt
@@ -2,24 +2,44 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationView
 
-class home : AppCompatActivity() {
+class home : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        // Default fragment to display
-        if (savedInstanceState == null) {
+        drawerLayout = findViewById(R.id.drawerLayout)
+
+        val drawerNavView = findViewById<NavigationView>(R.id.drawerNavigationView)
+        drawerNavView.setNavigationItemSelectedListener(this)
+
+        val toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open_nav,R.string.close_nav)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        if(savedInstanceState == null){
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container,Dashboard()).commit()
+            drawerNavView.setCheckedItem(R.id.nav_home)
+
             openDashboardFragment()
         }
 
-        val addBtn2 = findViewById<FloatingActionButton>(R.id.searchBtn)
+        val searchButton = findViewById<FloatingActionButton>(R.id.searchBtn)
 
-        addBtn2.setOnClickListener {
+        searchButton.setOnClickListener {
             val intent = Intent(this, search::class.java)
             startActivity(intent)
         }
@@ -39,6 +59,34 @@ class home : AppCompatActivity() {
                 }
                 else -> false
             }
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_home -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container,Dashboard()).commit()
+            R.id.nav_addItem -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container,AddItem()).commit()
+            R.id.nav_searchItem -> {
+                val intent = Intent(this, search::class.java)
+                startActivity(intent)
+            }
+            R.id.nav_info -> {
+                val intent = Intent(this, info::class.java)
+                startActivity(intent)
+            }
+            R.id.nav_logout -> Toast.makeText(this,"Logout",Toast.LENGTH_SHORT).show()
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            onBackPressedDispatcher.onBackPressed()
         }
     }
 
